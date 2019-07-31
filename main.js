04581461e1e60dacf921d83c7644abb59eb07130
likho-dyantyi <52657087+likho-dyantyi@users.noreply.github.com>
2019-07-31 16:47:08 +0200

    Add files via upload
    firebase database

---
+++ main.js
@@ -0,0 +1,47 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyDwlaCrpWOuWius26Xcrnk5cQfwVl6RVhE",
+  authDomain: "contactform-7e4b1.firebaseapp.com",
+  databaseURL: "https://contactform-7e4b1.firebaseio.com",
+  projectId: "contactform-7e4b1",
+  storageBucket: "",
+  messagingSenderId: "995461833612",
+  appId: "1:995461833612:web:95ce99fce0a423aa"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+//Refference messeges collection
+var messagesRef = firebase.database().ref("messages");
+
+//Listen for form submit
+document.getElementById("frmDataEntry").addEventListener("submit", submitForm);
+
+//submit form
+function submitForm(e) {
+  e.preventDefault();
+
+  //Get values
+  var name = getInputval("name");
+  var email = getInputval("email");
+  var message = getInputval("message");
+
+  //save message
+  saveMessage(name, email, message);
+}
+
+//function to get form values
+function getInputval(id) {
+  return document.getElementById(id).value;
+}
+
+//save message to firebase
+function saveMessage(name, email, message) {
+  var newMessageref = messagesRef.push();
+  newMessageref.set({
+    name: name,
+    email: email,
+    message: message
+  });
+}
